182f083c5e255ff39be93b76ecbf628b1a7b45a3
khaled hussien <57367756+khaled2024@users.noreply.github.com>
2022-12-19 14:48:16 +0200

    Initial Commit

---
+++ Food Ordering App/Food_Ordering_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_Ordering_AppApp.swift
+//  Food Ordering App
+//
+//  Created by KhaleD HuSsien on 19/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Food_Ordering_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
